d38cfbb2bd2730f6340871d7268e528c599df6de
Alex Petrescu <alexpetrescu1996@gmail.com>
2020-05-02 00:38:40 +0300

    added more unit tests

--- src/csharp-coding-playground.unit-tests/DataStructures/BinarySearchTreeTests.cs
+++ src/csharp-coding-playground.unit-tests/DataStructures/BinarySearchTreeTests.cs
@@ -181,5 +181,61 @@ namespace csharp_coding_playground.unit_tests.DataStructures
             Assert.AreEqual(9, array.ElementAt(11));
             Assert.AreEqual(7, array.ElementAt(12));
         }
+
+        [Test]
+        public void ShouldReturnCorrectResultOnDFSWhenStrategyIsNotProvided()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(9);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(2);
+            bst.Insert(11);
+            bst.Insert(12);
+            bst.Insert(10);
+            bst.Insert(6);
+            bst.Insert(8);
+            bst.Insert(1);
+            bst.Insert(0);
+
+            var array = bst.DFS();
+
+            Assert.AreEqual(0, array.ElementAt(0));
+            Assert.AreEqual(1, array.ElementAt(1));
+            Assert.AreEqual(2, array.ElementAt(2));
+            Assert.AreEqual(3, array.ElementAt(3));
+            Assert.AreEqual(4, array.ElementAt(4));
+            Assert.AreEqual(5, array.ElementAt(5));
+            Assert.AreEqual(6, array.ElementAt(6));
+            Assert.AreEqual(7, array.ElementAt(7));
+            Assert.AreEqual(8, array.ElementAt(8));
+            Assert.AreEqual(9, array.ElementAt(9));
+            Assert.AreEqual(10, array.ElementAt(10));
+            Assert.AreEqual(11, array.ElementAt(11));
+            Assert.AreEqual(12, array.ElementAt(12));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionOnDFSWhenStrategyIsUnknow()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(9);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(2);
+            bst.Insert(11);
+            bst.Insert(12);
+            bst.Insert(10);
+            bst.Insert(6);
+            bst.Insert(8);
+            bst.Insert(1);
+            bst.Insert(0);
+
+            Assert.Throws<Exception>(() => bst.DFS((DFSStrategy)(-1)));
+        }
     }
 }
