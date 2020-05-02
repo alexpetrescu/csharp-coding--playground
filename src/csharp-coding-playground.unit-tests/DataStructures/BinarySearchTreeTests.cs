22f15dbb07a8b2ec77b0b2c4b1f88e72dbd19558
Alex Petrescu <alexpetrescu1996@gmail.com>
2020-05-02 15:12:23 +0300

    added unit tests + bst

--- src/csharp-coding-playground.unit-tests/DataStructures/BinarySearchTreeTests.cs
+++ src/csharp-coding-playground.unit-tests/DataStructures/BinarySearchTreeTests.cs
@@ -8,6 +8,30 @@ namespace csharp_coding_playground.unit_tests.DataStructures
     public class BinarySearchTreeTests
     {
         [Test]
+        public void ShouldThrowExceptionOnInsertWhenValueExists()
+        {
+            var bst = new BinarySearchTree<int>();
+
+            bst.Insert(7);
+            Assert.Throws<Exception>(() => bst.Insert(7));
+
+            bst.Insert(9);
+            Assert.Throws<Exception>(() => bst.Insert(7));
+            Assert.Throws<Exception>(() => bst.Insert(9));
+
+            bst.Insert(4);
+            Assert.Throws<Exception>(() => bst.Insert(9));
+            Assert.Throws<Exception>(() => bst.Insert(7));
+            Assert.Throws<Exception>(() => bst.Insert(4));
+
+            bst.Insert(3);
+            Assert.Throws<Exception>(() => bst.Insert(9));
+            Assert.Throws<Exception>(() => bst.Insert(7));
+            Assert.Throws<Exception>(() => bst.Insert(4));
+            Assert.Throws<Exception>(() => bst.Insert(3));
+        }
+
+        [Test]
         public void ShouldAddElementAtCorrectPositionOnInsert()
         {
             var bst = new BinarySearchTree<int>();
@@ -27,6 +51,7 @@ namespace csharp_coding_playground.unit_tests.DataStructures
 
             var array = bst.DFS(DFSStrategy.InOrder);
 
+            Assert.AreEqual(13, array.Length);
             Assert.AreEqual(0, array.ElementAt(0));
             Assert.AreEqual(1, array.ElementAt(1));
             Assert.AreEqual(2, array.ElementAt(2));
@@ -62,6 +87,7 @@ namespace csharp_coding_playground.unit_tests.DataStructures
 
             var array = bst.BFS();
 
+            Assert.AreEqual(13, array.Length);
             Assert.AreEqual(7, array.ElementAt(0));
             Assert.AreEqual(4, array.ElementAt(1));
             Assert.AreEqual(9, array.ElementAt(2));
@@ -97,6 +123,7 @@ namespace csharp_coding_playground.unit_tests.DataStructures
 
             var array = bst.DFS(DFSStrategy.InOrder);
 
+            Assert.AreEqual(13, array.Length);
             Assert.AreEqual(0, array.ElementAt(0));
             Assert.AreEqual(1, array.ElementAt(1));
             Assert.AreEqual(2, array.ElementAt(2));
@@ -132,6 +159,7 @@ namespace csharp_coding_playground.unit_tests.DataStructures
 
             var array = bst.DFS(DFSStrategy.PreOrder);
 
+            Assert.AreEqual(13, array.Length);
             Assert.AreEqual(7, array.ElementAt(0));
             Assert.AreEqual(4, array.ElementAt(1));
             Assert.AreEqual(3, array.ElementAt(2));
@@ -167,6 +195,7 @@ namespace csharp_coding_playground.unit_tests.DataStructures
 
             var array = bst.DFS(DFSStrategy.PostOrder);
 
+            Assert.AreEqual(13, array.Length);
             Assert.AreEqual(0, array.ElementAt(0));
             Assert.AreEqual(1, array.ElementAt(1));
             Assert.AreEqual(2, array.ElementAt(2));
@@ -202,6 +231,7 @@ namespace csharp_coding_playground.unit_tests.DataStructures
 
             var array = bst.DFS();
 
+            Assert.AreEqual(13, array.Length);
             Assert.AreEqual(0, array.ElementAt(0));
             Assert.AreEqual(1, array.ElementAt(1));
             Assert.AreEqual(2, array.ElementAt(2));
@@ -236,6 +266,264 @@ namespace csharp_coding_playground.unit_tests.DataStructures
             bst.Insert(0);
 
             Assert.Throws<Exception>(() => bst.DFS((DFSStrategy)(-1)));
+        }
+
+        [Test]
+        public void ShouldReturnCorrectValueOnSearchWhenValueExists()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(9);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(2);
+            bst.Insert(11);
+            bst.Insert(12);
+            bst.Insert(10);
+            bst.Insert(6);
+            bst.Insert(8);
+            bst.Insert(1);
+            bst.Insert(0);
+
+            Assert.AreEqual(0, bst.Search(0));
+            Assert.AreEqual(1, bst.Search(1));
+            Assert.AreEqual(2, bst.Search(2));
+            Assert.AreEqual(3, bst.Search(3));
+            Assert.AreEqual(4, bst.Search(4));
+            Assert.AreEqual(5, bst.Search(5));
+            Assert.AreEqual(6, bst.Search(6));
+            Assert.AreEqual(7, bst.Search(7));
+            Assert.AreEqual(8, bst.Search(8));
+            Assert.AreEqual(9, bst.Search(9));
+            Assert.AreEqual(10, bst.Search(10));
+            Assert.AreEqual(11, bst.Search(11));
+            Assert.AreEqual(12, bst.Search(12));
+        }
+
+        [Test]
+        public void ShouldReturnDefaultValueOnSeachWhenValueDoesNotExist()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(9);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(2);
+            bst.Insert(11);
+            bst.Insert(12);
+            bst.Insert(10);
+            bst.Insert(6);
+            bst.Insert(8);
+            bst.Insert(1);
+            bst.Insert(0);
+
+
+            Assert.AreEqual(default(int), bst.Search(200));
+        }
+
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementExists()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(9);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(2);
+            bst.Insert(11);
+            bst.Insert(12);
+            bst.Insert(10);
+            bst.Insert(6);
+            bst.Insert(8);
+            bst.Insert(1);
+            bst.Insert(0);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+
+            Assert.AreEqual(13, array.Length);
+            Assert.AreEqual(0, array.ElementAt(0));
+            Assert.AreEqual(1, array.ElementAt(1));
+            Assert.AreEqual(2, array.ElementAt(2));
+            Assert.AreEqual(3, array.ElementAt(3));
+            Assert.AreEqual(4, array.ElementAt(4));
+            Assert.AreEqual(5, array.ElementAt(5));
+            Assert.AreEqual(6, array.ElementAt(6));
+            Assert.AreEqual(7, array.ElementAt(7));
+            Assert.AreEqual(8, array.ElementAt(8));
+            Assert.AreEqual(9, array.ElementAt(9));
+            Assert.AreEqual(10, array.ElementAt(10));
+            Assert.AreEqual(11, array.ElementAt(11));
+            Assert.AreEqual(12, array.ElementAt(12));
+
+            bst.Remove(7);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(12, array.Length);
+            Assert.AreEqual(0, array.ElementAt(0));
+            Assert.AreEqual(1, array.ElementAt(1));
+            Assert.AreEqual(2, array.ElementAt(2));
+            Assert.AreEqual(3, array.ElementAt(3));
+            Assert.AreEqual(4, array.ElementAt(4));
+            Assert.AreEqual(5, array.ElementAt(5));
+            Assert.AreEqual(6, array.ElementAt(6));
+            Assert.AreEqual(8, array.ElementAt(7));
+            Assert.AreEqual(9, array.ElementAt(8));
+            Assert.AreEqual(10, array.ElementAt(9));
+            Assert.AreEqual(11, array.ElementAt(10));
+            Assert.AreEqual(12, array.ElementAt(11));
+
+            bst.Remove(0);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(11, array.Length);
+            Assert.AreEqual(1, array.ElementAt(0));
+            Assert.AreEqual(2, array.ElementAt(1));
+            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(4, array.ElementAt(3));
+            Assert.AreEqual(5, array.ElementAt(4));
+            Assert.AreEqual(6, array.ElementAt(5));
+            Assert.AreEqual(8, array.ElementAt(6));
+            Assert.AreEqual(9, array.ElementAt(7));
+            Assert.AreEqual(10, array.ElementAt(8));
+            Assert.AreEqual(11, array.ElementAt(9));
+            Assert.AreEqual(12, array.ElementAt(10));
+
+            bst.Remove(12);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(10, array.Length);
+            Assert.AreEqual(1, array.ElementAt(0));
+            Assert.AreEqual(2, array.ElementAt(1));
+            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(4, array.ElementAt(3));
+            Assert.AreEqual(5, array.ElementAt(4));
+            Assert.AreEqual(6, array.ElementAt(5));
+            Assert.AreEqual(8, array.ElementAt(6));
+            Assert.AreEqual(9, array.ElementAt(7));
+            Assert.AreEqual(10, array.ElementAt(8));
+            Assert.AreEqual(11, array.ElementAt(9));
+
+            bst.Remove(6);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(9, array.Length);
+            Assert.AreEqual(1, array.ElementAt(0));
+            Assert.AreEqual(2, array.ElementAt(1));
+            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(4, array.ElementAt(3));
+            Assert.AreEqual(5, array.ElementAt(4));
+            Assert.AreEqual(8, array.ElementAt(5));
+            Assert.AreEqual(9, array.ElementAt(6));
+            Assert.AreEqual(10, array.ElementAt(7));
+            Assert.AreEqual(11, array.ElementAt(8));
+
+            bst.Remove(5);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(8, array.Length);
+            Assert.AreEqual(1, array.ElementAt(0));
+            Assert.AreEqual(2, array.ElementAt(1));
+            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(4, array.ElementAt(3));
+            Assert.AreEqual(8, array.ElementAt(4));
+            Assert.AreEqual(9, array.ElementAt(5));
+            Assert.AreEqual(10, array.ElementAt(6));
+            Assert.AreEqual(11, array.ElementAt(7));
+
+            bst.Remove(4);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(7, array.Length);
+            Assert.AreEqual(1, array.ElementAt(0));
+            Assert.AreEqual(2, array.ElementAt(1));
+            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(8, array.ElementAt(3));
+            Assert.AreEqual(9, array.ElementAt(4));
+            Assert.AreEqual(10, array.ElementAt(5));
+            Assert.AreEqual(11, array.ElementAt(6));
+
+            bst.Remove(8);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(6, array.Length);
+            Assert.AreEqual(1, array.ElementAt(0));
+            Assert.AreEqual(2, array.ElementAt(1));
+            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(9, array.ElementAt(3));
+            Assert.AreEqual(10, array.ElementAt(4));
+            Assert.AreEqual(11, array.ElementAt(5));
+
+            bst.Remove(9);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(5, array.Length);
+            Assert.AreEqual(1, array.ElementAt(0));
+            Assert.AreEqual(2, array.ElementAt(1));
+            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(10, array.ElementAt(3));
+            Assert.AreEqual(11, array.ElementAt(4));
+
+            bst.Remove(1);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(4, array.Length);
+            Assert.AreEqual(2, array.ElementAt(0));
+            Assert.AreEqual(3, array.ElementAt(1));
+            Assert.AreEqual(10, array.ElementAt(2));
+            Assert.AreEqual(11, array.ElementAt(3));
+
+            bst.Remove(3);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(3, array.Length);
+            Assert.AreEqual(2, array.ElementAt(0));
+            Assert.AreEqual(10, array.ElementAt(1));
+            Assert.AreEqual(11, array.ElementAt(2));
+
+            bst.Remove(10);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(2, array.Length);
+            Assert.AreEqual(2, array.ElementAt(0));
+            Assert.AreEqual(11, array.ElementAt(1));
+
+            bst.Remove(11);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(1, array.Length);
+            Assert.AreEqual(2, array.ElementAt(0));
+
+            bst.Remove(2);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(0, array.Length);
+        }
+
+        [Test]
+        public void ShouldNotDeleteAnythingOnRemoveWhenElementDoesNotExist()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(9);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(2);
+            bst.Insert(11);
+            bst.Insert(12);
+            bst.Insert(10);
+            bst.Insert(6);
+            bst.Insert(8);
+            bst.Insert(1);
+            bst.Insert(0);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            bst.Remove(200);
+
+            Assert.AreEqual(13, array.Length);
+            Assert.AreEqual(0, array.ElementAt(0));
+            Assert.AreEqual(1, array.ElementAt(1));
+            Assert.AreEqual(2, array.ElementAt(2));
+            Assert.AreEqual(3, array.ElementAt(3));
+            Assert.AreEqual(4, array.ElementAt(4));
+            Assert.AreEqual(5, array.ElementAt(5));
+            Assert.AreEqual(6, array.ElementAt(6));
+            Assert.AreEqual(7, array.ElementAt(7));
+            Assert.AreEqual(8, array.ElementAt(8));
+            Assert.AreEqual(9, array.ElementAt(9));
+            Assert.AreEqual(10, array.ElementAt(10));
+            Assert.AreEqual(11, array.ElementAt(11));
+            Assert.AreEqual(12, array.ElementAt(12));
         }
     }
 }
--- src/csharp-coding-playground/DataStructures/BinarySearchTree.cs
+++ src/csharp-coding-playground/DataStructures/BinarySearchTree.cs
@@ -12,7 +12,7 @@ namespace csharp_coding_playground.DataStructures
         private BinarySearchTreeNode<T> root = null;
 
         /// <summary>
-        /// Inserts a new node in the tree.
+        /// Inserts a new node with the given value in the tree.
         /// </summary>
         /// <param name="value"></param>
         public void Insert(T value) => root = Insert(value, root);
@@ -31,10 +31,135 @@ namespace csharp_coding_playground.DataStructures
             }
 
             var check = node.Value.CompareTo(value);
-            if (check >= 0) node.Left = Insert(value, node.Left);
+            if (check == 0) throw new Exception("Value already exists in the tree.");
+
+            if (check > 0) node.Left = Insert(value, node.Left);
             else node.Right = Insert(value, node.Right);
 
             return node;
+        }
+
+        /// <summary>
+        /// Searches for the given value in the tree.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public T Search(T value)
+        {
+            (_, var result) = Search(value, null, root);
+            if (result == null) return default;
+
+            return result.Value;
+        }
+
+        /// <summary>
+        /// Searches for the given value in the tree from the given node.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        private (BinarySearchTreeNode<T> parent, BinarySearchTreeNode<T> node) Search(T value, BinarySearchTreeNode<T> parent, BinarySearchTreeNode<T> node)
+        {
+            if (node == null) return (parent, null);
+
+            var compare = node.Value.CompareTo(value);
+            if (compare == 0) return (parent, node);
+            else if (compare > 0) return Search(value, node, node.Left);
+            else return Search(value, node, node.Right);
+        }
+
+        /// <summary>
+        /// Removes value from the tree.
+        /// </summary>
+        /// <param name="value"></param>
+        public void Remove(T value)
+        {
+            (var parent, var node) = Search(value, null, root);
+            Remove(parent, node);
+        }
+
+        /// <summary>
+        /// Removes node from the tree.
+        /// </summary>
+        /// <param name="value"></param>
+        private void Remove(BinarySearchTreeNode<T> parent, BinarySearchTreeNode<T> node)
+        {
+            if (node == null) return;
+
+            if (node.Left == null && node.Right == null)
+            {
+                if (node == root)
+                {
+                    root = null;
+                }
+                else
+                {
+                    if (parent.Left == node) parent.Left = null;
+                    if (parent.Right == node) parent.Right = null;
+                }
+
+                return;
+            }
+
+            if (node.Left == null)
+            {
+                if (node == root)
+                {
+                    root = node.Right;
+                }
+                else
+                {
+                    if (parent.Left == node) parent.Left = node.Right;
+                    if (parent.Right == node) parent.Right = node.Right;
+                }
+
+                return;
+            }
+
+            if (node.Right == null)
+            {
+                if (node == root)
+                {
+                    root = node.Left;
+                }
+                else
+                {
+                    if (parent.Left == node) parent.Left = node.Left;
+                    if (parent.Right == node) parent.Right = node.Left;
+                }
+
+                return;
+            }
+
+            if (node.Right != null && node.Left != null)
+            {
+                (var minParent, var min) = Min(node, node.Right);
+                if (node == root)
+                {
+                    root.Value = min.Value;
+                    Remove(minParent, min);
+                }
+                else
+                {
+                    node.Value = min.Value;
+                    Remove(minParent, min);
+                }
+
+                return;
+            }
+
+        }
+
+        /// <summary>
+        /// Returns the min node from the given node.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        private (BinarySearchTreeNode<T> parent, BinarySearchTreeNode<T> node) Min(BinarySearchTreeNode<T> parent, BinarySearchTreeNode<T> node)
+        {
+            if (node.Left == null) return (parent, node);
+            return Min(node, node.Left);
         }
 
         /// <summary>
