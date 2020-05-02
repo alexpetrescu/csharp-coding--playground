b1ff38dcaf7e31347bb1d730e70427f7223a21da
Alex Petrescu <alexpetrescu1996@gmail.com>
2020-05-02 16:13:10 +0300

    improved unit tests

--- src/csharp-coding-playground.unit-tests/DataStructures/BinarySearchTreeTests.cs
+++ src/csharp-coding-playground.unit-tests/DataStructures/BinarySearchTreeTests.cs
@@ -324,169 +324,356 @@ namespace csharp_coding_playground.unit_tests.DataStructures
         }
 
         [Test]
-        public void ShouldRemoveCorrectElementOnRemoveWhenElementExists()
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasNoChildAndIsNotRoot()
         {
             var bst = new BinarySearchTree<int>();
             bst.Insert(7);
             bst.Insert(9);
             bst.Insert(4);
-            bst.Insert(5);
-            bst.Insert(3);
-            bst.Insert(2);
-            bst.Insert(11);
-            bst.Insert(12);
-            bst.Insert(10);
-            bst.Insert(6);
-            bst.Insert(8);
-            bst.Insert(1);
-            bst.Insert(0);
+            
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 3);
+            Assert.AreEqual(4, array.ElementAt(0));
+            Assert.AreEqual(7, array.ElementAt(1));
+            Assert.AreEqual(9, array.ElementAt(2));
+
+            bst.Remove(4);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 2);
+            Assert.AreEqual(7, array.ElementAt(0));
+            Assert.AreEqual(9, array.ElementAt(1));
+        }
+
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasNoChildAndIsRoot()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
 
             var array = bst.DFS(DFSStrategy.InOrder);
-
-            Assert.AreEqual(13, array.Length);
-            Assert.AreEqual(0, array.ElementAt(0));
-            Assert.AreEqual(1, array.ElementAt(1));
-            Assert.AreEqual(2, array.ElementAt(2));
-            Assert.AreEqual(3, array.ElementAt(3));
-            Assert.AreEqual(4, array.ElementAt(4));
-            Assert.AreEqual(5, array.ElementAt(5));
-            Assert.AreEqual(6, array.ElementAt(6));
-            Assert.AreEqual(7, array.ElementAt(7));
-            Assert.AreEqual(8, array.ElementAt(8));
-            Assert.AreEqual(9, array.ElementAt(9));
-            Assert.AreEqual(10, array.ElementAt(10));
-            Assert.AreEqual(11, array.ElementAt(11));
-            Assert.AreEqual(12, array.ElementAt(12));
+            Assert.AreEqual(array.Length, 1);
+            Assert.AreEqual(7, array.ElementAt(0));
 
             bst.Remove(7);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(12, array.Length);
-            Assert.AreEqual(0, array.ElementAt(0));
-            Assert.AreEqual(1, array.ElementAt(1));
-            Assert.AreEqual(2, array.ElementAt(2));
-            Assert.AreEqual(3, array.ElementAt(3));
-            Assert.AreEqual(4, array.ElementAt(4));
-            Assert.AreEqual(5, array.ElementAt(5));
-            Assert.AreEqual(6, array.ElementAt(6));
-            Assert.AreEqual(8, array.ElementAt(7));
-            Assert.AreEqual(9, array.ElementAt(8));
-            Assert.AreEqual(10, array.ElementAt(9));
-            Assert.AreEqual(11, array.ElementAt(10));
-            Assert.AreEqual(12, array.ElementAt(11));
+            Assert.AreEqual(array.Length, 0);
+        }
 
-            bst.Remove(0);
-            array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(11, array.Length);
-            Assert.AreEqual(1, array.ElementAt(0));
-            Assert.AreEqual(2, array.ElementAt(1));
-            Assert.AreEqual(3, array.ElementAt(2));
-            Assert.AreEqual(4, array.ElementAt(3));
-            Assert.AreEqual(5, array.ElementAt(4));
-            Assert.AreEqual(6, array.ElementAt(5));
-            Assert.AreEqual(8, array.ElementAt(6));
-            Assert.AreEqual(9, array.ElementAt(7));
-            Assert.AreEqual(10, array.ElementAt(8));
-            Assert.AreEqual(11, array.ElementAt(9));
-            Assert.AreEqual(12, array.ElementAt(10));
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasOneChildOnLeftAndIsNotRoot()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(4);
+            bst.Insert(3);
+            bst.Insert(8);
 
-            bst.Remove(12);
-            array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(10, array.Length);
-            Assert.AreEqual(1, array.ElementAt(0));
-            Assert.AreEqual(2, array.ElementAt(1));
-            Assert.AreEqual(3, array.ElementAt(2));
-            Assert.AreEqual(4, array.ElementAt(3));
-            Assert.AreEqual(5, array.ElementAt(4));
-            Assert.AreEqual(6, array.ElementAt(5));
-            Assert.AreEqual(8, array.ElementAt(6));
-            Assert.AreEqual(9, array.ElementAt(7));
-            Assert.AreEqual(10, array.ElementAt(8));
-            Assert.AreEqual(11, array.ElementAt(9));
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 4);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(4, array.ElementAt(1));
+            Assert.AreEqual(7, array.ElementAt(2));
+            Assert.AreEqual(8, array.ElementAt(3));
 
-            bst.Remove(6);
+            bst.Remove(4);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(9, array.Length);
-            Assert.AreEqual(1, array.ElementAt(0));
-            Assert.AreEqual(2, array.ElementAt(1));
-            Assert.AreEqual(3, array.ElementAt(2));
-            Assert.AreEqual(4, array.ElementAt(3));
-            Assert.AreEqual(5, array.ElementAt(4));
-            Assert.AreEqual(8, array.ElementAt(5));
-            Assert.AreEqual(9, array.ElementAt(6));
-            Assert.AreEqual(10, array.ElementAt(7));
-            Assert.AreEqual(11, array.ElementAt(8));
+            Assert.AreEqual(array.Length, 3);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(7, array.ElementAt(1));
+            Assert.AreEqual(8, array.ElementAt(2));
+        }
 
-            bst.Remove(5);
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasOneChildOnLeftAndIsRoot()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(3);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 2);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(7, array.ElementAt(1));
+
+            bst.Remove(7);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(8, array.Length);
-            Assert.AreEqual(1, array.ElementAt(0));
-            Assert.AreEqual(2, array.ElementAt(1));
-            Assert.AreEqual(3, array.ElementAt(2));
-            Assert.AreEqual(4, array.ElementAt(3));
+            Assert.AreEqual(array.Length, 1);
+            Assert.AreEqual(3, array.ElementAt(0));
+        }
+
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasOneChildOnRightAndIsNotRoot()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(8);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 4);
+            Assert.AreEqual(4, array.ElementAt(0));
+            Assert.AreEqual(5, array.ElementAt(1));
+            Assert.AreEqual(7, array.ElementAt(2));
+            Assert.AreEqual(8, array.ElementAt(3));
+
+            bst.Remove(4);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 3);
+            Assert.AreEqual(5, array.ElementAt(0));
+            Assert.AreEqual(7, array.ElementAt(1));
+            Assert.AreEqual(8, array.ElementAt(2));
+        }
+
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasOneChildOnRightAndIsRoot()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(8);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 2);
+            Assert.AreEqual(7, array.ElementAt(0));
+            Assert.AreEqual(8, array.ElementAt(1));
+
+            bst.Remove(7);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 1);
+            Assert.AreEqual(8, array.ElementAt(0));
+        }
+
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasTwoChildrenAndIsNotRoot()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(4);
+            bst.Insert(5);
+            bst.Insert(6);
+            bst.Insert(8);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 5);
+            Assert.AreEqual(4, array.ElementAt(0));
+            Assert.AreEqual(5, array.ElementAt(1));
+            Assert.AreEqual(6, array.ElementAt(2));
+            Assert.AreEqual(7, array.ElementAt(3));
+            Assert.AreEqual(8, array.ElementAt(4));
+
+            bst.Remove(4);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 4);
+            Assert.AreEqual(5, array.ElementAt(0));
+            Assert.AreEqual(6, array.ElementAt(1));
+            Assert.AreEqual(7, array.ElementAt(2));
+            Assert.AreEqual(8, array.ElementAt(3));
+        }
+
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasTwoChildrenAndIsRoot()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(7);
+            bst.Insert(8);
+            bst.Insert(6);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 3);
+            Assert.AreEqual(6, array.ElementAt(0));
+            Assert.AreEqual(7, array.ElementAt(1));
+            Assert.AreEqual(8, array.ElementAt(2));
+
+            bst.Remove(7);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 2);
+            Assert.AreEqual(6, array.ElementAt(0));
+            Assert.AreEqual(8, array.ElementAt(1));
+        }
+
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasTwoChildrenWithChildrenAndIsNotRoot()
+        {
+            // Right -> Right case
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(10);
+            bst.Insert(4);
+            bst.Insert(3);
+            bst.Insert(5);
+            bst.Insert(6);
+            bst.Insert(11);
+            bst.Insert(12);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 7);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(4, array.ElementAt(1));
+            Assert.AreEqual(5, array.ElementAt(2));
+            Assert.AreEqual(6, array.ElementAt(3));
+            Assert.AreEqual(10, array.ElementAt(4));
+            Assert.AreEqual(11, array.ElementAt(5));
+            Assert.AreEqual(12, array.ElementAt(6));
+
+            bst.Remove(4);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 6);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(5, array.ElementAt(1));
+            Assert.AreEqual(6, array.ElementAt(2));
+            Assert.AreEqual(10, array.ElementAt(3));
+            Assert.AreEqual(11, array.ElementAt(4));
+            Assert.AreEqual(12, array.ElementAt(5));
+
+            // Right -> Left -> Right case
+            bst = new BinarySearchTree<int>();
+            bst.Insert(10);
+            bst.Insert(4);
+            bst.Insert(3);
+            bst.Insert(8);
+            bst.Insert(6);
+            bst.Insert(7);
+            bst.Insert(9);
+            bst.Insert(11);
+            bst.Insert(12);
+
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 9);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(4, array.ElementAt(1));
+            Assert.AreEqual(6, array.ElementAt(2));
+            Assert.AreEqual(7, array.ElementAt(3));
             Assert.AreEqual(8, array.ElementAt(4));
             Assert.AreEqual(9, array.ElementAt(5));
             Assert.AreEqual(10, array.ElementAt(6));
             Assert.AreEqual(11, array.ElementAt(7));
+            Assert.AreEqual(12, array.ElementAt(8));
 
             bst.Remove(4);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(7, array.Length);
-            Assert.AreEqual(1, array.ElementAt(0));
-            Assert.AreEqual(2, array.ElementAt(1));
-            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(array.Length, 8);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(6, array.ElementAt(1));
+            Assert.AreEqual(7, array.ElementAt(2));
             Assert.AreEqual(8, array.ElementAt(3));
             Assert.AreEqual(9, array.ElementAt(4));
             Assert.AreEqual(10, array.ElementAt(5));
             Assert.AreEqual(11, array.ElementAt(6));
+            Assert.AreEqual(12, array.ElementAt(7));
 
-            bst.Remove(8);
-            array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(6, array.Length);
-            Assert.AreEqual(1, array.ElementAt(0));
-            Assert.AreEqual(2, array.ElementAt(1));
-            Assert.AreEqual(3, array.ElementAt(2));
-            Assert.AreEqual(9, array.ElementAt(3));
-            Assert.AreEqual(10, array.ElementAt(4));
-            Assert.AreEqual(11, array.ElementAt(5));
+            // Right and Left case
+            bst = new BinarySearchTree<int>();
+            bst.Insert(10);
+            bst.Insert(4);
+            bst.Insert(3);
+            bst.Insert(5);
+            bst.Insert(11);
+            bst.Insert(12);
 
-            bst.Remove(9);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(5, array.Length);
-            Assert.AreEqual(1, array.ElementAt(0));
-            Assert.AreEqual(2, array.ElementAt(1));
-            Assert.AreEqual(3, array.ElementAt(2));
+            Assert.AreEqual(array.Length, 6);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(4, array.ElementAt(1));
+            Assert.AreEqual(5, array.ElementAt(2));
             Assert.AreEqual(10, array.ElementAt(3));
             Assert.AreEqual(11, array.ElementAt(4));
+            Assert.AreEqual(12, array.ElementAt(5));
 
-            bst.Remove(1);
+            bst.Remove(4);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(4, array.Length);
-            Assert.AreEqual(2, array.ElementAt(0));
-            Assert.AreEqual(3, array.ElementAt(1));
+            Assert.AreEqual(array.Length, 5);
+            Assert.AreEqual(3, array.ElementAt(0));
+            Assert.AreEqual(5, array.ElementAt(1));
             Assert.AreEqual(10, array.ElementAt(2));
             Assert.AreEqual(11, array.ElementAt(3));
+            Assert.AreEqual(12, array.ElementAt(4));
+        }
 
-            bst.Remove(3);
-            array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(3, array.Length);
-            Assert.AreEqual(2, array.ElementAt(0));
-            Assert.AreEqual(10, array.ElementAt(1));
-            Assert.AreEqual(11, array.ElementAt(2));
+        [Test]
+        public void ShouldRemoveCorrectElementOnRemoveWhenElementHasTwoChildrenWithChildrenAndIsRoot()
+        {
+            // Right -> Left -> Right case
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(10);
+            bst.Insert(9);
+            bst.Insert(8);
+            bst.Insert(13);
+            bst.Insert(11);
+            bst.Insert(12);
+
+            var array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 6);
+            Assert.AreEqual(8, array.ElementAt(0));
+            Assert.AreEqual(9, array.ElementAt(1));
+            Assert.AreEqual(10, array.ElementAt(2));
+            Assert.AreEqual(11, array.ElementAt(3));
+            Assert.AreEqual(12, array.ElementAt(4));
+            Assert.AreEqual(13, array.ElementAt(5));
 
             bst.Remove(10);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(2, array.Length);
-            Assert.AreEqual(2, array.ElementAt(0));
-            Assert.AreEqual(11, array.ElementAt(1));
+            Assert.AreEqual(array.Length, 5);
+            Assert.AreEqual(8, array.ElementAt(0));
+            Assert.AreEqual(9, array.ElementAt(1));
+            Assert.AreEqual(11, array.ElementAt(2));
+            Assert.AreEqual(12, array.ElementAt(3));
+            Assert.AreEqual(13, array.ElementAt(4));
 
-            bst.Remove(11);
-            array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(1, array.Length);
-            Assert.AreEqual(2, array.ElementAt(0));
+            // Right and Left case
+            bst = new BinarySearchTree<int>();
+            bst.Insert(11);
+            bst.Insert(9);
+            bst.Insert(8);
+            bst.Insert(10);
+            bst.Insert(14);
+            bst.Insert(13);
+            bst.Insert(15);
 
-            bst.Remove(2);
             array = bst.DFS(DFSStrategy.InOrder);
-            Assert.AreEqual(0, array.Length);
+            Assert.AreEqual(array.Length, 7);
+            Assert.AreEqual(8, array.ElementAt(0));
+            Assert.AreEqual(9, array.ElementAt(1));
+            Assert.AreEqual(10, array.ElementAt(2));
+            Assert.AreEqual(11, array.ElementAt(3));
+            Assert.AreEqual(13, array.ElementAt(4));
+            Assert.AreEqual(14, array.ElementAt(5));
+            Assert.AreEqual(15, array.ElementAt(6));
+
+            bst.Remove(10);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 6);
+            Assert.AreEqual(8, array.ElementAt(0));
+            Assert.AreEqual(9, array.ElementAt(1));
+            Assert.AreEqual(11, array.ElementAt(2));
+            Assert.AreEqual(13, array.ElementAt(3));
+            Assert.AreEqual(14, array.ElementAt(4));
+            Assert.AreEqual(15, array.ElementAt(5));
+
+            // Right -> Right case
+            bst = new BinarySearchTree<int>();
+            bst.Insert(11);
+            bst.Insert(9);
+            bst.Insert(8);
+            bst.Insert(10);
+            bst.Insert(14);
+            bst.Insert(15);
+
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 6);
+            Assert.AreEqual(8, array.ElementAt(0));
+            Assert.AreEqual(9, array.ElementAt(1));
+            Assert.AreEqual(10, array.ElementAt(2));
+            Assert.AreEqual(11, array.ElementAt(3));
+            Assert.AreEqual(14, array.ElementAt(4));
+            Assert.AreEqual(15, array.ElementAt(5));
+
+            bst.Remove(10);
+            array = bst.DFS(DFSStrategy.InOrder);
+            Assert.AreEqual(array.Length, 5);
+            Assert.AreEqual(8, array.ElementAt(0));
+            Assert.AreEqual(9, array.ElementAt(1));
+            Assert.AreEqual(11, array.ElementAt(2));
+            Assert.AreEqual(14, array.ElementAt(3));
+            Assert.AreEqual(15, array.ElementAt(4));
         }
 
         [Test]
